191151d6041a18f533435db962ab27937c582f22
Steffen Gericke <bad_boy_99@hotmail.de>
2011-06-05 03:04:34 +0200

    filename update

--- mvc/init.php
+++ mvc/init.php
@@ -1,7 +1,7 @@
 <?php
     defined(SYS_PATH)  or define("SYS_PATH", dirname(__FILE__)."/");
  
-    require_once SYS_PATH . "functions/basic.php";
+    require_once SYS_PATH . "functions/basicloader.php";
     require_once SYS_PATH . "classes/loader.php";
     
     Loader::addAutoLoad("_classLoad");
