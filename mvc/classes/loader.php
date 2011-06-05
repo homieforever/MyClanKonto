570018741c40acd889251d2070733137c82a7a21
Steffen Gericke <bad_boy_99@hotmail.de>
2011-06-05 02:41:25 +0200

    loader.php added

---
+++ mvc/classes/loader.php
@@ -0,0 +1,25 @@
+<?php
+    class loader
+    {
+        public function addAutoLoad($function = NULL)
+        {
+            if($function != NULL && is_callback($function))
+            {
+                return spl_autoload_register($function);
+            }
+            else
+            {
+                throw new Exception('loader->addAutoLoad(): Es wurde ein nicht gültiger Callback angegeben.');
+            }
+        }
+        
+        public function unsetAutoLoad($function = NULL)
+        {
+            if($function != NULL)
+            {
+                return spl_autoload_unregister($function);
+            }
+            return false;
+        }
+    }
+?>
