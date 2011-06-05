115b2faaa84fec5b131e41822145c5f10d6e23c2
Steffen Gericke <bad_boy_99@hotmail.de>
2011-06-05 18:29:05 +0200

    add Routermap

---
+++ mvc/maps/routerMap.php
@@ -0,0 +1,6 @@
+<?php
+    $values = array("/:clanid/:bereich/" => array('controller' => 'site', 'action' => ''),
+                    "/:clanid/:bereich/:site/" => array('controller' => 'site', 'action' => ''),
+                    "/:clanid/:bereich/:site/:id", array('controller' => 'site', 'action' => '')
+                    );
+?>
