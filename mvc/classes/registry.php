7de5a40be630ed9aa21d31a943ab2f0696047623
Steffen Gericke <bad_boy_99@hotmail.de>
2011-06-05 11:38:10 +0200

    Added the Registry Class.

---
+++ mvc/classes/registry.php
@@ -0,0 +1,71 @@
+<?php
+    abstract class Registry
+    {
+        private $values = array();
+        
+        public function __construct()
+        {
+             session_start();
+             if(isset($_SESSION['_registry']) === FALSE)
+             {
+                 $_SESSION['_registry'] = array();
+             }
+        }
+        
+        public function add($key, $value, $scope = false)
+        {
+            if($scope)
+            {
+                if(!isset($_SESSION['_registry'][$key]))
+                {
+                    $_SESSION['_registry'][$key] = $value;
+                    return true;
+                }    
+            }
+            else
+            {
+                if(!isset($this->values[$key]))
+                {
+                    $this->values[$key]= $value;
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        public function get($key)
+        {
+            if(isset($_SESSION['_registry'][$key]))
+            {
+                 return $_SESSION['_registry'][$key];
+            }
+            elseif(isset($this->values[$key]))
+            {
+                return $this->values[$key];
+            }
+            return NULL;
+        }
+        
+        public function unadd($key)
+        {
+            if(isset($_SESSION['_registry'][$key]))
+            {
+                unset($_SESSION['_registry'][$key]);
+                return true;
+            }
+            elseif(isset($this->values[$key]))
+            {
+                unset($this->values[$key]);
+                return true;
+            }
+            return false;    
+        }
+        
+        public function reset()
+        {
+            $_SESSION['_registry'] = array();
+            $this->values = array();
+            return true;
+        }
+    }
+?>
